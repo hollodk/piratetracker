c4e698cc5eeff0668af34ce77f378dc6c1f98825
Michael Holm <hollo@hollo.dk>
2016-04-21 16:26:49 +0200

    Missing helper.

--- src/Hollo/TrackerBundle/Helper/CoordinateHelper.php
+++ src/Hollo/TrackerBundle/Helper/CoordinateHelper.php
@@ -8,17 +8,17 @@ class CoordinateHelper
     {
         $route = array();
 
-        $route[] = array('lat' => 57.037972, 'lon' => 9.945928);
-        $route[] = array('lat' => 57.038198, 'lon' => 9.948483);
-        $route[] = array('lat' => 57.038976, 'lon' => 9.948213);
-        $route[] = array('lat' => 57.039236, 'lon' => 9.950825);
-        $route[] = array('lat' => 57.040428, 'lon' => 9.950127);
-        $route[] = array('lat' => 57.040678, 'lon' => 9.951832);
-        $route[] = array('lat' => 57.041536, 'lon' => 9.951243);
-        $route[] = array('lat' => 57.043842, 'lon' => 9.949548);
-        $route[] = array('lat' => 57.044553, 'lon' => 9.948330);
-        $route[] = array('lat' => 57.045277, 'lon' => 9.946417);
-        $route[] = array('lat' => 57.046292, 'lon' => 9.946069);
+        $route[] = array('lat' => 57.037972, 'lng' => 9.945928);
+        $route[] = array('lat' => 57.038198, 'lng' => 9.948483);
+        $route[] = array('lat' => 57.038976, 'lng' => 9.948213);
+        $route[] = array('lat' => 57.039236, 'lng' => 9.950825);
+        $route[] = array('lat' => 57.040428, 'lng' => 9.950127);
+        $route[] = array('lat' => 57.040678, 'lng' => 9.951832);
+        $route[] = array('lat' => 57.041536, 'lng' => 9.951243);
+        $route[] = array('lat' => 57.043842, 'lng' => 9.949548);
+        $route[] = array('lat' => 57.044553, 'lng' => 9.948330);
+        $route[] = array('lat' => 57.045277, 'lng' => 9.946417);
+        $route[] = array('lat' => 57.046292, 'lng' => 9.946069);
 
         return $route;
     }
@@ -31,26 +31,26 @@ class CoordinateHelper
     {
         $route = array();
 
-        $route[] = array('lat' => 57.046232, 'lon' => 9.944152);
-        $route[] = array('lat' => 57.046667, 'lon' => 9.943945);
-        $route[] = array('lat' => 57.046729, 'lon' => 9.942812);
-        $route[] = array('lat' => 57.046873, 'lon' => 9.938995);
-        $route[] = array('lat' => 57.046925, 'lon' => 9.934474);
-        $route[] = array('lat' => 57.046894, 'lon' => 9.932432);
-        $route[] = array('lat' => 57.047045, 'lon' => 9.931282);
-        $route[] = array('lat' => 57.048372, 'lon' => 9.926516);
-        $route[] = array('lat' => 57.048765, 'lon' => 9.926214);
-        $route[] = array('lat' => 57.049551, 'lon' => 9.925467);
-        $route[] = array('lat' => 57.050230, 'lon' => 9.924189);
-        $route[] = array('lat' => 57.051264, 'lon' => 9.920382);
-        $route[] = array('lat' => 57.051713, 'lon' => 9.919271);
-        $route[] = array('lat' => 57.050984, 'lon' => 9.918633);
-        $route[] = array('lat' => 57.051293, 'lon' => 9.916752);
-        $route[] = array('lat' => 57.049291, 'lon' => 9.916188);
-        $route[] = array('lat' => 57.047500, 'lon' => 9.915102);
-        $route[] = array('lat' => 57.046385, 'lon' => 9.913544);
-        $route[] = array('lat' => 57.045010, 'lon' => 9.912332);
-        $route[] = array('lat' => 57.042575, 'lon' => 9.911002);
+        $route[] = array('lat' => 57.046232, 'lng' => 9.944152);
+        $route[] = array('lat' => 57.046667, 'lng' => 9.943945);
+        $route[] = array('lat' => 57.046729, 'lng' => 9.942812);
+        $route[] = array('lat' => 57.046873, 'lng' => 9.938995);
+        $route[] = array('lat' => 57.046925, 'lng' => 9.934474);
+        $route[] = array('lat' => 57.046894, 'lng' => 9.932432);
+        $route[] = array('lat' => 57.047045, 'lng' => 9.931282);
+        $route[] = array('lat' => 57.048372, 'lng' => 9.926516);
+        $route[] = array('lat' => 57.048765, 'lng' => 9.926214);
+        $route[] = array('lat' => 57.049551, 'lng' => 9.925467);
+        $route[] = array('lat' => 57.050230, 'lng' => 9.924189);
+        $route[] = array('lat' => 57.051264, 'lng' => 9.920382);
+        $route[] = array('lat' => 57.051713, 'lng' => 9.919271);
+        $route[] = array('lat' => 57.050984, 'lng' => 9.918633);
+        $route[] = array('lat' => 57.051293, 'lng' => 9.916752);
+        $route[] = array('lat' => 57.049291, 'lng' => 9.916188);
+        $route[] = array('lat' => 57.047500, 'lng' => 9.915102);
+        $route[] = array('lat' => 57.046385, 'lng' => 9.913544);
+        $route[] = array('lat' => 57.045010, 'lng' => 9.912332);
+        $route[] = array('lat' => 57.042575, 'lng' => 9.911002);
 
         return $route;
     }
